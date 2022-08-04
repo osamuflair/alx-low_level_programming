31d33142dbd7e472ab406c9ea8c5f3ea096a23c3
osamudiamen <osamudiamenbenedict05@gmail.com>
2022-08-04 12:19:53 -0700

    The header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
