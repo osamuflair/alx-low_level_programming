36f931920f42437ea58d5063a1f60eb02135d6d9
osamudiamen <osamudiamenbenedict05@gmail.com>
2022-08-11 13:12:30 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	int count;
+	int count = 1;
 
 	if (h->str == 'NULL')
 	{
@@ -18,8 +18,9 @@ size_t print_list(const list_t *h)
 	{
 		while (h->str != 'NULL')
 		{
-			count = printf("[%d] (%s)\n", h->len, h->str);
+			printf("[%d] (%s)\n", h->len, h->str);
 			h = h->next;
+			count++;
 		}
 	return (count);
 }
