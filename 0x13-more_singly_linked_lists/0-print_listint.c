3142d508845c66479421526d8e2333fd983c8e27
osamudiamen <osamudiamenbenedict05@gmail.com>
2022-08-16 00:18:42 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 /**
  * print_listint - prints all the elements of a listint_t list.
  * @h: the firat arguement
- * Return :the number of nodes
+ * Return:the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
