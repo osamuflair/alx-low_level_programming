39d6c8987bef2638df291108b63598de27f42e8e
Osamudiamen <osamudiamenbenedict05@gmail.com>
2022-10-03 14:35:31 +0000

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - function to check for uppercase character
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
